ee78a0bccbd30986ae8553d0a0c8250d4c7e4a7b
jadhavprajakta2303-bit <jadhavprajakta2303@gmail.com>
2026-03-10 13:38:04 +0530

    Add files via upload

---
+++ exp4/act3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 13:36:24 2026
+
+@author: prajakta
+"""
+
+feedback = "Great service and fast delivery"
+
+reversed_feedback = feedback[::-1]
+
+print("Reversed Feedback:", reversed_feedback)
