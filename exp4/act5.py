8e3b5157b3db11c29e07aede9d9013da682833c7
jadhavprajakta2303-bit <jadhavprajakta2303@gmail.com>
2026-03-10 13:41:29 +0530

    Add files via upload

---
+++ exp4/act5.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 13:39:59 2026
+
+@author: prajakta
+"""
+
+numbers = [9876543210, 9123456780, 9876543210, 9988776655, 9123456780]
+
+unique_numbers = list(set(numbers))
+
+print("Unique Numbers:", unique_numbers)
