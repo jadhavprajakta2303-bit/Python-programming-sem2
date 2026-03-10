185c500884c8511b0eb1638faa9395a4578cddce
jadhavprajakta2303-bit <jadhavprajakta2303@gmail.com>
2026-03-10 13:34:19 +0530

    Add files via upload

---
+++ exp4/act1exp4.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 13:30:07 2026
+
+@author: prajakta
+"""
+
+marks = [78, 85, 90, 67, 88]
+
+average = sum(marks) / len(marks)
+topper = max(marks)
+
+print("Average Marks:", average)
+print("Topper Marks:", topper)
