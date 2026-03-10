3b7f687aa589cce27daba84e26c7c9c978a2170c
jadhavprajakta2303-bit <jadhavprajakta2303@gmail.com>
2026-03-10 13:35:40 +0530

    Add files via upload

---
+++ exp4/act2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 13:34:49 2026
+
+@author: prajakta
+"""
+
+prices = [250, 120, 75, 300, 90]
+
+total_bill = sum(prices)
+
+print("Total Bill:", total_bill)
+
