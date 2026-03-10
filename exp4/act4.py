a075e9e17c569b9430bf540e5729088e523cc7e4
jadhavprajakta2303-bit <jadhavprajakta2303@gmail.com>
2026-03-10 13:39:33 +0530

    Add files via upload

---
+++ exp4/act4.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 13:38:27 2026
+
+@author: prajakta
+"""
+
+review = "good product good quality good price"
+word = "good"
+
+count = review.count(word)
+
+print("Word appears:", count, "times")
