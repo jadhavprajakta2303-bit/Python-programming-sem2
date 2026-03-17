5b7e1917b0287a8907861b1cfff6f8483b35af9f
jadhavprajakta2303-bit <jadhavprajakta2303@gmail.com>
2026-03-17 13:19:29 +0530

    Add files via upload

---
+++ Exp5/exp5 act5.py
@@ -0,0 +1,13 @@
+# Common followers between two accounts 
+"""
+Created on Tue Mar 17 13:18:09 2026
+
+@author: prajakta jadhav
+"""
+
+account1 = {"rahul", "sneha", "amit", "pooja"}
+account2 = {"amit", "pooja", "karan"}
+
+common_followers = account1 & account2
+
+print(common_followers)
