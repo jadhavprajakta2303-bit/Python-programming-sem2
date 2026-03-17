8303c130596cb5fbe932cc1db554c50ae79e43f8
jadhavprajakta2303-bit <jadhavprajakta2303@gmail.com>
2026-03-17 13:36:14 +0530

    Add files via upload

---
+++ Exp1/exp1 act1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 13:34:23 2026
+
+@author: prajakta jadhav
+"""
+
+num1=int(input("Enter number1:"))
+num2=int(input("Enter number2:"))
+
+sum=num1+num2
+print("addition=",sum)
+
+sub=num1-num2
+print("substraction=",sub)
+
+mul=num1*num2
+print("multiplication=",mul)
+
+div=num1/num2
+print("division=",div)
+
+mod=num1%num2
+print("modulus=",mod)
