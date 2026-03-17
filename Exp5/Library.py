2d8de15ec4dff406597d61cfde4fdb03468f869b
jadhavprajakta2303-bit <jadhavprajakta2303@gmail.com>
2026-03-17 13:03:20 +0530

    Add files via upload

---
+++ Exp5/Library.py
@@ -0,0 +1,13 @@
+#1. Library dictionary – update book price
+
+"""
+Created on Tue Mar 17 13:01:02 2026
+
+@author: prajakta jadhav
+"""
+books = {"Math": 300, "Physics": 400, "Chemistry": 350}
+
+# Update price
+books["Physics"] = 450
+
+print(books)
