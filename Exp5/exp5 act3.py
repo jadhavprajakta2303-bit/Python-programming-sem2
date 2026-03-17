9d871cc908717362c37ff81de7aec57cb1ff3c3c
jadhavprajakta2303-bit <jadhavprajakta2303@gmail.com>
2026-03-17 13:14:33 +0530

    Add files via upload

---
+++ Exp5/exp5 act3.py
@@ -0,0 +1,16 @@
+#Shop inventory – add new stock
+"""
+Created on Tue Mar 17 13:13:12 2026
+
+@author: prajakta jadhav
+"""
+
+inventory = {"Apples": 50, "Bananas": 30}
+
+# Add new stock
+inventory["Oranges"] = 40
+
+# Update existing stock
+inventory["Apples"] += 20
+
+print(inventory)
