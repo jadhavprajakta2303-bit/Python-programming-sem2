9d8ce466d74bc6aea8eb00cc3acca655c65f0452
jadhavprajakta2303-bit <jadhavprajakta2303@gmail.com>
2026-03-24 13:21:00 +0530

    Add files via upload

---
+++ Exp6/exp6 act2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 24 13:19:07 2026
+
+@author: prajakta jadhav
+"""
+
+file = open("attendance.txt", "a")
+
+n = int(input("Enter number of students: "))
+for i in range(n):
+    name = input("Enter student name: ")
+    status = input("Present/Absent: ")
+    file.write(name + " - " + status + "\n")
+
+file.close()
+
+print("Attendance saved successfully.")
