97157491f488643b79c81b063122608e4820da17
jadhavprajakta2303-bit <jadhavprajakta2303@gmail.com>
2026-04-28 13:51:55 +0530

    Add files via upload

---
+++ Exp9/Exp9 act5.py
@@ -0,0 +1,13 @@
+# Generate OTP using random module.
+"""
+Created on Tue Apr 28 13:49:42 2026
+
+@author: Prajakta
+"""
+# 5. Generate OTP using random module
+
+import random
+
+otp = random.randint(1000, 9999)
+
+print("Generated OTP:", otp)
