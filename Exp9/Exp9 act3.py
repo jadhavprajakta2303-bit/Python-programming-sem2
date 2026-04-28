056938c3702b4507cb1bcc40c50aa6eb44195762
jadhavprajakta2303-bit <jadhavprajakta2303@gmail.com>
2026-04-28 13:49:25 +0530

    Add files via upload

---
+++ Exp9/Exp9 act3.py
@@ -0,0 +1,17 @@
+# 3. Calculate distance using math formulas
+
+"""
+Created on Tue Apr 28 13:43:31 2026
+
+@author: Prajakta
+"""
+import math
+
+x1 = float(input("Enter x1: "))
+y1 = float(input("Enter y1: "))
+x2 = float(input("Enter x2: "))
+y2 = float(input("Enter y2: "))
+
+distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+print("Distance =", distance)
