0ff8010299bddecda5bc3205d3bd43a31a28864c
jadhavprajakta2303-bit <jadhavprajakta2303@gmail.com>
2026-04-28 13:53:44 +0530

    Add files via upload

---
+++ Exp9/Exp9 act4.py
@@ -0,0 +1,15 @@
+# 4. Show weekday in office attendance app
+
+"""
+Created on Tue Apr 28 13:52:23 2026
+
+@author: Prajakta
+"""
+
+# 4. Show weekday in office attendance app
+
+import datetime
+
+today = datetime.datetime.today()
+
+print("Today is:", today.strftime("%A"))
